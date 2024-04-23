fd8779f59efb24b80028b06503056f669bffb9fc
DaviGeraldoDev <davi.geraldo.senai@gmail.com>
2024-04-23 15:25:53 -0300

    home page

--- src/pages/Principal/Principal.js
+++ src/pages/Principal/Principal.js
@@ -1,38 +1,141 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
 export default function Principal() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-      source = {require('../../assets/wave-haikei.png')}
-      style = {styles.imagebackground}
-      >
-        <View>
-            <Text style={styles.title}>Entrou</Text>
+        <View style={styles.header}>
+            <Image source={require('../../assets/logo_desenho.png')}
+            style = {styles.logo}
+            />
+            <View style={styles.icons_view}>
+              <Image source={require('../../assets/Olho.png')}
+              style = {styles.icons}
+              />
+              <Image source={require('../../assets/Busca.png')}
+              style = {styles.icons}
+              />
+              <Image source={require('../../assets/conta.png')}
+              style = {styles.icons}
+              />
+            </View>
         </View>
 
-      </ImageBackground>
-      
+        <View style={styles.saldo}>
+          <View style={styles.saldo_e_seta}>
+            <Text style={styles.saldo_text}>R$ 40.000,00</Text>
+            <Image source={require('../../assets/seta.png')}
+            style={styles.img_seta}/>
+          </View>
+            <View style={styles.formas_pagamentos}>
+
+              <View style={styles.caixa_pag}>
+                <Image source={require('../../assets/pix.png')}
+                  style = {styles.img_pag}
+                  />
+              </View>
+
+              <View style={styles.caixa_pag}>
+                <Image source={require('../../assets/cartao.png')}
+                  style = {styles.img_pag}
+                  />
+              </View>
+
+              <View style={styles.caixa_pag}>
+                <Image source={require('../../assets/boleto.png')}
+                  style = {styles.img_pag}
+                  />
+              </View>
+
+            </View>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-  },
-
-  imagebackground: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#fff'
+    // backgroundColor: '#181A1C'
   },
 
-  title: {
+  header: {
+    backgroundColor: '#002233',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    width: '100%',
+    height: 110,
+    flexDirection: 'row'
+  },
+
+  icons_view: {
+    flexDirection: 'row',
+    gap: 10
+  },
+
+  logo: {
+    width: 60,
+    height: 60
+  },
+
+  icons: {
+    width: 35,
+    height: 35
+  },
+
+  saldo: {
+    backgroundColor: '#002233',
+    marginTop: 40,
+    width: 340,
+    height: 190,
+    borderRadius: 40
+  },
+
+  saldo_text: {
     color: 'white',
-    fontSize: 20
+    fontSize: 30,
+    marginLeft: 30,
+    marginTop: 50
+  },
+
+  formas_pagamentos: {
+    marginTop: 30,
+    marginLeft: 30,
+    flexDirection: 'row',
+    gap: 10
+  },
+
+  img_pag: {
+    width: 35,
+    height: 35,
+
+  },
+
+  caixa_pag: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    height: '100%',
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15
+  },
+
+  img_seta: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginTop: 50,
+    marginLeft: 60,
+    width: 40,
+    height: 40
+  },
+
+  saldo_e_seta:{
+    flexDirection: 'row'
   }
+
 });
